ca2a2aa66caae40306754825647fe3f62527a294
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-04 23:47:22 +0900

    layout2

--- web/app/components/Header.js
+++ web/app/components/Header.js
@@ -1,7 +1,6 @@
-import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
-import React from 'react'
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import getRandomColor from './GetRandomColor'
 
 const useStyles = makeStyles(() => ({
   typographyStyles: {
@@ -9,16 +8,27 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
+function GetRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function Header() {
   const classes = useStyles();
+  const [color, setColor] = useState(() => GetRandomColor());
+
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{ backgroundColor: color }}>
             <Toolbar>
                 <Typography className={classes.typographyStyles}>Profile</Typography>
                 <StarBorderIcon />
             </Toolbar>
         </AppBar>
-    )
+    );
 }
 
-export default Header
+export default Header;
