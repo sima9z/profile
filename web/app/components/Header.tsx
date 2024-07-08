fe2418653f354dc3f1bd109ce83512f98a03d15f
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-08 19:01:00 +0900

    tsx

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -1,21 +1,13 @@
 import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
+import GetRandomColor from './getRandomColor' 
 
 const useStyles = makeStyles(() => ({
   typographyStyles: {
     flex: 1
   }
 }));
-
-function GetRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
 
 function Header() {
   const classes = useStyles();
