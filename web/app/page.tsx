32b77d3099f46629e64924fc0e36dc620bbe7434
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-10 20:22:42 +0900

    layoutAndPage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,17 @@
-import HomePage from './components/pages/HomePage';
+"use client";
+
+import React from 'react';
+
+import ProfileSection from './components/organisms/ProfileSection';
+import BackgroundImage from './components/atoms/BackgroundImage';
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <ProfileSection />
+      <BackgroundImage src="https://picsum.photos/2000" /> 
+    </>
+  );
+};
 
 export default HomePage;
